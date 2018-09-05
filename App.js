0d74fc198c3bf65d3d3c45bb1c5be1d0090a06be
austinloveless <austinloveless5171@gmail.com>
2018-09-05 12:04:32 -0600

    refactored app,js for initial set up

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>an app</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
